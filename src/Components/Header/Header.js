29ca005e0f12a65576514527d709eb226c4b9ca8
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-17 15:00:26 +0330

    update main content header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './NavBar'
 import './Header.css'
+import MainContentHeader from './MainContentHeader'
 
 
 function Header() {
   return (
     <div className='header-section'>
         <NavBar/>
+        <MainContentHeader/>
     </div>
   )
 }
---
+++ src/Components/Header/MainContentHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function MainContentHeader() {
+  return (
+    <div className='MainContentHeader'>
+        <h1 className='title-header'>Powerful digital solutions with</h1>
+        <h2 className='company-name-header'>Falcon</h2>
+        <p className='description-header'>We are a team of talented digital marketers</p>
+    </div>
+  )
+}
+
+export default MainContentHeader
