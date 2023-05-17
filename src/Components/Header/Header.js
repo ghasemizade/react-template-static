e8a89150fbf0a8a43251427c897419c354e9cf67
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-17 14:04:00 +0330

    update navbar style

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
 
+
 function Header() {
   return (
-    <NavBar/>
+    <div className='header-section'>
+        <NavBar/>
+    </div>
   )
 }
 
