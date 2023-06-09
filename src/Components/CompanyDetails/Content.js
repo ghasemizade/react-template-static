b3beb1674566ccd4bb7e2cd6f14e3692197d3c59
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-06-09 21:08:45 +0330

    import images

--- src/Components/CompanyDetails/Content.js
+++ src/Components/CompanyDetails/Content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Content.css'
+import about from '../../assets/about.jpg'
 
 function Content() {
   return (
@@ -10,7 +11,7 @@ function Content() {
                 <p>Noad Marketing Company, as a leading company in the digital marketing industry, is trying to play an important role in the economic prosperity of Iran with the advancement of digital business and internal works by using modern technical knowledge and providing new marketing and branding solutions. show As the most specialized digital market brand in Iran and expanding its name in global markets, Noad has taken steps to exploit services based on modern technologies. Noad owes its progress during a decade of activity to transparency, coherence and results to the predicted results of its foot. Investing in new technologies, adherence and quality to values, customer satisfaction and loyalty, human and dynamic human resources are the inherent values of Noad. The digital space has created a tremendous transformation in communication and how people interact with each other. The tools and platforms available in this era have increased the power of combing and choosing for people and made it easier to evaluate and compare different brands and products. We consider this change of attitude in choosing a brand and product as an opportunity for us and our customers. In the digital space, it is easy to identify the target market, discover their needs, desires, and interests, and use the information obtained to create and develop products, brands, and appropriate experiences that come from the integration of technology and creativity along with The marketing method pays. Novin comes, took action. At Noad, we are ready to accept new things and stick to our work at any cost.</p>
             </div>
             <div className='right-side-img'>
-                <img src={'about.jpg'} alt="about img" />
+                <img src={about} alt="about img" />
             </div>
         </div>
     </div>
--- src/Components/Header/BoxHeader.js
+++ src/Components/Header/BoxHeader.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import './Header.css'
-// import test from 'shop-solid.svg'
+import shop from '../../assets/shop-solid.svg'
+import chart from '../../assets/chart-simple-solid.svg'
+import calendar from '../../assets/calendar-solid.svg'
+import paint from '../../assets/paint-roller-solid.svg'
+import database from '../../assets/database-solid.svg'
 
 
 function BoxHeader() {
   return (
     <div className='boxes-header'>
         <div className="boxes">
-            <div className='icon-header'><img className='shop-solid' src={'shop-solid.svg'} alt="shop"/></div>
+            <div className='icon-header'><img className='shop-solid' src={shop} alt="shop"/></div>
             <p className='description-title'>Dramatic text</p>
         </div>
         <div className="boxes">
-            <div className='icon-header'><img src={'chart-simple-solid.svg'} alt="shop" /></div>
+            <div className='icon-header'><img src={chart} alt="shop" /></div>
             <p className='description-title'>Qualitative chart</p>
         </div>
         <div className="boxes">
-            <div className='icon-header'><img src={'calendar-solid.svg'} alt="shop" /></div>
+            <div className='icon-header'><img src={calendar} alt="shop" /></div>
             <p className='description-title'>portfolio</p>
         </div>
         <div className="boxes">
-            <div className='icon-header'><img src={'paint-roller-solid.svg'} alt="shop" /></div>
+            <div className='icon-header'><img src={paint} alt="shop" /></div>
             <p className='description-title'>Final functions</p>
         </div>
         <div className="boxes">
-            <div className='icon-header'><img src={'database-solid.svg'} alt="shop" /></div>
+            <div className='icon-header'><img src={database} alt="shop" /></div>
             <p className='description-title'>Stored information</p>
         </div>
     </div>
